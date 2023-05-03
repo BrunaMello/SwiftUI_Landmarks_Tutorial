8a64df62e19051a873ab68d0d055cacc96e81376
Bruna Mello <brunacrespomello@gmail.com>
2023-05-03 08:15:46 +0100

    Initial Commit

---
+++ SwiftUI_Landmarks_Tutorial/SwiftUI_Landmarks_TutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Landmarks_TutorialApp.swift
+//  SwiftUI_Landmarks_Tutorial
+//
+//  Created by Bruna Mello on 03/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Landmarks_TutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
