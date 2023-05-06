420025ddbd0bbda6347a9f8d9edd3a5329f79273
Bruna Mello <brunacrespomello@gmail.com>
2023-05-06 17:30:11 +0100

    04. Drawing Paths and Shapes

---
+++ SwiftUI_Landmarks_Tutorial/Badge/RotatedBadgeSymbol.swift
@@ -0,0 +1,25 @@
+//
+//  RotatedBadgeSymbol.swift
+//  SwiftUI_Landmarks_Tutorial
+//
+//  Created by Bruna Mello on 04/05/2023.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+        
+    }
+}
+
+struct RotatedBadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        RotatedBadgeSymbol(angle: Angle(degrees: 5))
+    }
+}
